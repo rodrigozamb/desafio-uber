ce03f3c42c0d3b5a8886a4bf783104fb7d821b5e
rodrigozamb <rodrigo-zamboni@hotmail.com>
2024-09-06 18:38:39 -0300

    initial commit

---
+++ src/main/java/com/rodrigozamboni/desafio_uber/DesafioUberApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigozamboni.desafio_uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigozamboni/desafio_uber/DesafioUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigozamboni.desafio_uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
